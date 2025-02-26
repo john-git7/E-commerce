1866b91cac8c2cdc1380fc022e25a1f87353ae25
john-git7 <gideonebenezer467@gmail.com>
2025-02-26 14:22:29 +0530

    Delete frontend directory

--- frontend/src/Routes/Routes.jsx
+++
@@ -1,6 +0,0 @@
-import LoginPage from "../Page/LoginPage";
-import SignupPage from "../Page/SignupPage";
-import Home from "../Page/Home";
-import CreateProduct from "../Page/Createproduct";  
-
-export { LoginPage, SignupPage, Home, CreateProduct }; 
--- frontend/src/server.js
+++
@@ -1,2 +0,0 @@
-const server = "http://localhost:5000"
-export default server
