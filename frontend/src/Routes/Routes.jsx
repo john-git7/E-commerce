a0e141bc954b51f8dddcb94939d5cee9b41e3cad
john-git7 <gideonebenezer467@gmail.com>
2025-02-27 11:09:02 +0530

    done

---
+++ frontend/src/Routes/Routes.jsx
@@ -0,0 +1,6 @@
+import LoginPage from "../Page/LoginPage";
+import SignupPage from "../Page/SignupPage";
+import Home from "../Page/Home";
+import CreateProduct from "../Page/Createproduct";  
+
+export { LoginPage, SignupPage, Home, CreateProduct }; 
